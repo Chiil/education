5fee9de8a01718a895d660486687f188bf24768c
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2016-06-07 16:01:00 +0200

    Added first draft Geostrophic wind practical

--- dynamic_meteorology/data/get_data.py
+++ dynamic_meteorology/data/get_data.py
@@ -11,7 +11,7 @@ server.retrieve({
     'step'      : "0",
     'grid'      : "0.75/0.75",
     'time'      : "00",
-    'date'      : "2005-08-29/to/2005-08-29",
+    'date'      : "1990-01-25/to/1990-01-25",
     'type'      : "an",
     'class'     : "ei",
     'format'    : "netcdf",
